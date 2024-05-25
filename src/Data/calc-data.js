2350be64a49775f86b40a1aa99a9318fcc71c26d
Anthony Climer <anthonyclimer0925@gmail.com>
2024-05-24 20:15:55 -0400

    About done adding the data for the react app

--- src/Data/calc-data.js
+++ src/Data/calc-data.js
@@ -2,6 +2,11 @@ export const word = 'hello'
 
 export default [
     {
-        id:'key-1'
+        id:'key-1',
+        key_info:'1'
+    },
+    {
+        id:'key-2',
+        key_info:'1'
     }
 ]

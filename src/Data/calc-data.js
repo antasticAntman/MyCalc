2ea104c4debc1d59fa6c4aac414195993b7c2858
Anthony Climer <anthonyclimer0925@gmail.com>
2024-05-27 17:22:56 -0400

    Added some information regarding the dummy data

--- src/Data/calc-data.js
+++ src/Data/calc-data.js
@@ -7,6 +7,31 @@ export default [
     },
     {
         id:'key-2',
-        key_info:'1'
-    }
+        key_info:'2'
+    },
+    {
+        id:'key-3',
+        key_info:'3'
+    },
+    {
+        id:'key-4',
+        key_info:'4'
+    },    {
+        id:'key-5',
+        key_info:'5'
+    },    {
+        id:'key-6',
+        key_info:'6'
+    },
+    {
+        id:'key-7',
+        key_info:'7'
+    },
+    {
+        id:'key-8',
+        key_info:'8'
+    },    {
+        id:'key-9',
+        key_info:'9'
+    },
 ]

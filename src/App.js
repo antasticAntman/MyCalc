fc2821b89d9a9daa85b7db43384de4e793e80e54
Anthony Climer <anthonyclimer0925@gmail.com>
2024-05-29 14:51:12 -0400

    Added the test data for the calculator

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import React, {useEffect, useState} from 'react'
 import calc_data, {word} from './Data/calc-data'
+import CalcKey from './Components/CalcKey';
 
 function App() {
 
+  const testData = calc_data[0]
+
   useEffect( () => {
     console.log(`Making sure the react app is mounting correcty and ${word}`)
-    console.log(calc_data)
+    console.log(testData)
   })
 
   return (
   <div className="calc-body">
     <h1>My Calc</h1>
+    <CalcKey calc_data={testData}/>
   </div>
   )
 }
--- src/Components/CalcKey.jsx
+++ src/Components/CalcKey.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-export default function CalcKey(id, name) {
-     
+export default function CalcKey(props) {
+    
+    const {calc_data} = props
+
+    useEffect( () =>  {
+        console.log('button', 'button data is', props.calc_data)
+    })
     
     return (
-        <button id={id}>{name}</button>
+        <button id={calc_data.id}>{calc_data.key_info}</button>
      );
 };

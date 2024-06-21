bb0e212c84d0b309ad7cbffe368dba4c6b8aea86
Anthony Climer <anthonyclimer0925@gmail.com>
2024-06-20 22:40:30 -0400

    Added the function through the parent to the child

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import CalcKey from './Components/CalcKey';
 
 function App() {
 
-
-  useEffect( () => {
-  })
+  const buttonClicker = () => {
+    return (
+    console.log('hello from the calc')
+    )
+}
 
   return (
   <div className='calcApp'>
@@ -23,7 +25,7 @@ function App() {
         <div className='grid-container grid-container-keysection'>
           {calc_data.map((calc_info) => {
             return (
-              <CalcKey calc_data={calc_info}/>        
+              <CalcKey calc_data={calc_info} buttonClicker={buttonClicker}/>        
             )
           })}
         </div>
--- src/Components/CalcKey.jsx
+++ src/Components/CalcKey.jsx
@@ -2,15 +2,19 @@ import React, { useEffect } from 'react'
 
 export default function CalcKey(props) {
     
+    useEffect( () => {
+        console.log(props)
+    })
+
     const {calc_data} = props
 
-    const buttonClicker = () => {
-        return (
-        console.log('hello from the calc')
-        )
-    }
+    // const buttonClicker = () => {
+    //     return (
+    //     console.log('hello from the calc')
+    //     )
+    // }
     
     return (
-        <button className='uppercase bg-color-light key text-white' id={calc_data.id} onClick={ () => {buttonClicker()}}>{calc_data.key_info}</button>
+        <button className='uppercase bg-color-light key text-white' id={calc_data.id}>{calc_data.key_info}</button>
      );
 };

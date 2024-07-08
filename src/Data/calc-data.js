c73b5359d6f8968cb8b532930158483f18230f43
Anthony Climer <anthonyclimer0925@gmail.com>
2024-07-07 20:40:43 -0400

    I am updating the dummy data to work better with the buttons

--- src/Data/calc-data.js
+++ src/Data/calc-data.js
@@ -3,39 +3,49 @@ export const word = 'hello'
 export default [
     {
         id:'key-1',
-        key_info:'1'
+        key_info:'1',
+        key_data:'1'
     },
     {
         id:'key-2',
-        key_info:'2'
+        key_info:'2',
+        key_data:'2'
     },
     {
         id:'key-3',
-        key_info:'3'
+        key_info:'3',
+        key_data:'3',
     },
     {
         id:'key-4',
-        key_info:'4'
+        key_info:'4',
+        key_data:'4'
     },    {
         id:'key-5',
-        key_info:'5'
+        key_info:'5',
+        key_data:'5'
     },    {
         id:'key-6',
-        key_info:'6'
+        key_info:'6',
+        key_data:'6',
     },
     {
         id:'key-7',
-        key_info:'7'
+        key_info:'7',
+        key_data:'7',
     },
     {
         id:'key-8',
-        key_info:'8'
+        key_info:'8',
+        key_data:'8',
     },    {
         id:'key-9',
-        key_info:'9'
+        key_info:'9',
+        key_data:'9'
     },    {
         id:'key-negative',
-        key_info:'+/-'
+        key_info:'+/-',
+        key_data:'-'
     },    {
         id:'key-0',
         key_info:'0'

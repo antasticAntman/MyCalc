47920e4bb4b634bc164fa38e8a78d50dc6d7d62c
Anthony Climer <anthonyclimer0925@gmail.com>
2024-08-30 21:32:51 -0400

    I have set up the check for the results

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ function App() {
     } else {
       // I want to check if the results have been updated!
       // If the results count if greater than one, set the equation back to a empty string, then set the count to zero
-      
+        if(count === 1){
+          console.log(' the results have been determined!')
+        }
 
       setEquation(equation + number)
       // console.log('count',count)

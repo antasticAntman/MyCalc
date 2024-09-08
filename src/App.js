c582ca9028e4f19d70cdc0ee40be754ec1a249ab
Anthony Climer <anthonyclimer0925@gmail.com>
2024-09-07 20:26:14 -0400

    Finalized the booleans

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       // I want to check if the results have been updated!
       // If the results count if greater than one, set the equation back to a empty string, then set the count to zero
       // *VERY IMPORTANT Make a conditional that ignores the booleans after results have been calculated* 
-        if(count === 1 && (number !== "*" || number !== "-")){
+        if(count === 1 && (number !== "*" || number !== "-" || number !== '+' || number !== '/')){
           console.log('entered this mofo')
          setEquation(number)
          setCount(0) 
